0f61c04b011ed6b0e2614147935aef6ba0f50ffa
Orumgbe <orumgbeugbekile@gmail.com>
2023-04-21 17:34:30 -0700

    create files

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# Creates a file with given requirements
+file { 'school':
+    ensure  => file,
+    path    => '/tmp/school',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love puppet',
+}
