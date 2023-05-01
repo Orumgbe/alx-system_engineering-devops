244e43c732426480393daefddef3e874ff88803b
Orumgbe <orumgbeugbekile@gmail.com>
2023-05-01 06:22:53 -0700

    puppet configuration

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+# configure nginx
+
+package { nginx :
+    ensure => 'installed',
+}
+
+file { /var/www/html/index.nginx-debian.html :
+    require => Package['nginx'],
+    content => 'Hello World!',
+}
+
+file_line { 'add-rewrite' :
+    ensure => 'present',
+    require => Package['nginx'],
+    path => '/etc/nginx/sites-enabled/default',
+    after => 'root /var/www/html;',
+    line => 'rewrite ^/redirect_me sitesurf.tech permanent;',
+    notify => Service['nginx'],
+}
+
+service { 'nginx' :
+    ensure => running,
+    require => File_line['add-rewrite'],
+}
