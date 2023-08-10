a9a2b4da2065db86a1a8756b0c21628c21cca568
Orumgbe <orumgbeugbekile@gmail.com>
2023-08-10 03:50:11 -0700

    recursive query with pagination

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,21 +5,31 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """Returns list of titles of hot articles in subreddit"""
-    url = "http://api.reddit.com/r/{}/hot?after={}".format(subreddit, after)
-    header = {
-        "User-Agent": "/u/k4rma_sutra"
-    }
-    r = requests.get(url, headers=header)
-    if r.status_code != 200:
-        print(r.status_code)
-        return None
+    """
+        returns a list of the hottest posts of a subreddit
+        or None
+    """
+    u_agent = {'User-Agent': '/u/recurseIT'}
+    url = 'https://api.reddit.com/r/{}/hot?after={}'.format(subreddit, after)
+    res = requests.get(url,  headers=u_agent)
+    h_list = []
+    if res.status_code == 200:
+        hottest = res.json()["data"]["children"]
+        after = res.json()["data"]["after"]
 
-    listings = r.json()['data']['children']
-    for post in listings:
-        hot_list.append(post['data']['title'])
-    after = r.json()['data']['after']
-    if after is not None:
-        return recurse(subreddit, hot_list, after)
+        if after is None:
+            h_list = append_page(hottest, len(hottest))
+            return h_list
+        h_list.append(recurse(subreddit, h_list, after=after))
+        h_list = append_page(hottest, len(hottest))
     else:
-        return hot_list
+        return None
+    return h_list
+
+
+def append_page(h_list, count, new_hlist=[]):
+    """append each data in the page"""
+    if count == 0:
+        return new_hlist
+    new_hlist.append(h_list[count - 1]["data"]["title"])
+    return (append_page(h_list, count - 1, new_hlist))
