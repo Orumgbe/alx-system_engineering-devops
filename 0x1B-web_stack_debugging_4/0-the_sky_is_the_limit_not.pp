a5d2c9d835f0f97d35e6d692b07f709487b34d28
Orumgbe <orumgbeugbekile@gmail.com>
2023-09-19 08:42:02 -0700

    fix

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,8 @@
 # change nginx open file limits
 exec { 'change limit':
-  command => 'sudo sed -i "s/15/4096/g" /etc/default/nginx'
+  command => 'sudo sed -i "s/15/4096/g" /etc/default/nginx',
 }
 
 exec { 'restart nginx':
-  command    => 'sudo service nginx restart'
+  command    => 'sudo service nginx restart',
 }
