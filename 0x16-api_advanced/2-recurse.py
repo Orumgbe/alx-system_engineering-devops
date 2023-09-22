ecdafe55200432e9605d65c7cdd7787bb12dc72d
Orumgbe <orumgbeugbekile@gmail.com>
2023-09-22 14:02:21 -0700

    recurse

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,32 +1,39 @@
 #!/usr/bin/python3
-"""List of title of all hot articles in given subreddit"""
-
+"""
+    Query the Reddit API and returns a list of the hottest post for
+    a given subreddit
+"""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """Queries subreddit for hottest articles"""
+    """
+        Returns a list of the hottest posts of a subreddit
+        or None
+    """
+    u_agent = {'User-Agent': '/u/k4rma_sutra'}
     url = 'https://api.reddit.com/r/{}/hot?after={}'.format(subreddit, after)
-    agent = {'User-Agent': '/u/k4rma_sutra'}
-    res = requests.get(url,  headers=agent)
+    res = requests.get(url,  headers=u_agent)
     h_list = []
     if res.status_code == 200:
         hottest = res.json()["data"]["children"]
         after = res.json()["data"]["after"]
 
         if after is None:
-            h_list = append_page(hottest, len(hottest))
+            h_list = get_children(hottest, len(hottest))
             return h_list
         h_list.append(recurse(subreddit, h_list, after=after))
-        h_list = append_page(hottest, len(hottest))
+        h_list = get_children(hottest, len(hottest))
     else:
         return None
     return h_list
 
 
-def append_page(h_list, count, new_hlist=[]):
-    """append each data in the page"""
+def get_children(h_list, count, new_hlist=[]):
+    """
+        gets the children from the data
+    """
     if count == 0:
         return new_hlist
     new_hlist.append(h_list[count - 1]["data"]["title"])
-    return (append_page(h_list, count - 1, new_hlist))
+    return (get_children(h_list, count - 1, new_hlist))
